85f40402053ffcf98a62ed3a20cfdb7c5a08d907
Hilalnigarbasan <hilalnigarbasan@outlook.com>
2023-01-31 04:54:18 +0300

    30.01.23

--- src/test/java/techproed/tests/Day20_DependsOnMethods.java
+++ src/test/java/techproed/tests/Day20_DependsOnMethods.java
@@ -1,2 +1,34 @@
-package techproed.tests;public class Day20_DependsOnMethods {
+package techproed.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Day20_DependsOnMethods {
+
+    @Test
+    public void homeTest(){
+        System.out.println("Home Test");
+        Assert.assertTrue(false); //FAIL
+    }
+    @Test(dependsOnMethods = "homeTest")
+    public void searchTest(){
+        System.out.println("Search Test");
+    }
+    @Test(dependsOnMethods = "homeTest")
+    public void paymentTest(){
+        System.out.println("Payment Test");
+
+    }
+
 }
+
+/*
+TestNG de tum  @Test metotlar bitbirlerinden bagimsiz olarak calisirlar
+Eger methodlari bagimli hale getirmek istersem, dependsOnMethods parametresi kullanilir
+Burdaki ornekte, searchTest methodu homeTest methoduna bagimlidir
+searchTest den once homeTest calisir
+Eger homeTest basarili ise searchTest calisir
+Eget homeTest basarisiz ise, searchTest IGNORE edilir
+*/
+
+//homeTest fail olduğu için çalışmadı
